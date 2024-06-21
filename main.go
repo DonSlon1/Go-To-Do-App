d98bbc84a112ff7c9d45d173684d15619d045147
DonSlon1 <lukindihel@gmail.com>
2024-06-21 10:15:13 +0200

    init

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package toDoApp
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	myWindow.SetContent(hello)
+
+	myWindow.ShowAndRun()
+}
